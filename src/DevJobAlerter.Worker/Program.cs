d6cac157ed8924481a0739fb4ac4dd033a5192cc
Leonardo Loss da Silva <leonardolossdasilva@gmail.com>
2026-07-16 12:27:35 -0300

    feat: implement worker service and configure dependency injection

--- src/DevJobAlerter.Worker/Program.cs
+++ src/DevJobAlerter.Worker/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DevJobAlerter.Domain.Interfaces;
+using DevJobAlerter.Infrastructure.Services;
+using DevJobAlerter.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+// Alterado aqui para usar o caminho completo e evitar ambiguidades
+var builder = Microsoft.Extensions.Hosting.Host.CreateApplicationBuilder(args);
+
+// 1. Dependency Injection Setup
+builder.Services.AddSingleton<INotificationService, WhatsAppNotificationService>();
+
+// 2. Register the Worker as a Hosted Service
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
